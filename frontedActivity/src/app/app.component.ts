bef602e0c434b54458c0e7591acb0bcae5a58d26
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2024-04-10 19:18:03 -0500

    Update app.component.ts

--- frontedActivity/src/app/app.component.ts
+++ frontedActivity/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Route, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'frontedActivity';
-}
+};
+
