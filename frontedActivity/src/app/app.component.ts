43fd12f9e640ab3eb533fe5ce9af3a1951d4a7d0
felipeGustin <dfgustin.4560@unicesmag.edu.co>
2024-04-12 01:18:33 -0500

    Update app.component.ts

--- frontedActivity/src/app/app.component.ts
+++ frontedActivity/src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { Route, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent,NavbarComponent],
+  imports: [RouterOutlet,LoginComponent,NavbarComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   title = 'fronted Activity';
+  
 };
 
