6365e45aefb37a46c342768d50bd307555960683
felipeGustin <dfgustin.4560@unicesmag.edu.co>
2024-04-12 02:11:03 -0500

    Update navbar.component.ts

--- frontedActivity/src/app/components/navbar/navbar.component.ts
+++ frontedActivity/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
-import { Route } from '@angular/router';
+import { Route, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent,RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
