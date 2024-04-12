735c3d7f5db458a841d933da946cd814521c540c
felipeGustin <dfgustin.4560@unicesmag.edu.co>
2024-04-12 02:01:42 -0500

    Update app.routes.ts

--- frontedActivity/src/app/app.routes.ts
+++ frontedActivity/src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
-    {path:'home',component:HomeComponent}
+    {path:'home',component:HomeComponent},
+    
 ];
